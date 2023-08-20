358d79a3474438d2418a3d94462437c07875fac8
amlan2211 <amlankmishra@gmail.com>
2023-08-20 12:54:57 +0530

    Commit

---
+++ src/test/java/com/tourism/nielmart/module1/Admin.java
@@ -0,0 +1,18 @@
+package com.tourism.nielmart.module1;
+
+import org.testng.annotations.Test;
+
+public class Admin {
+	@Test
+	public void display()
+	{
+		System.out.println("admin module is ready");
+	}
+	@Test
+	public void modify()
+	{
+		System.out.println("admin module is modified");
+	}
+	
+
+}
---
+++ src/test/java/com/tourism/nielmart/module1/Buyer.java
@@ -0,0 +1,16 @@
+package com.tourism.nielmart.module1;
+
+import org.testng.annotations.Test;
+
+public class Buyer {
+	@Test
+	public void buy()
+        {
+		System.out.println("buyer is ready ");
+		}
+   @Test
+   public void sell()
+   {
+	   System.out.println(" seller is ready");
+   }
+}
